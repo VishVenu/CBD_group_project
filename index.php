bff92d30d60a27b5657fddea91e5c54474f4403e
Parwat Kunwar <parwatkunwar08@gmail.com>
2022-05-18 13:27:16 -0400

    OrderIndex - List the order details with actions

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     </style>
     <script type="text/javascript">
         $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
+            $('[data-toggle="tooltip"]').tooltip();
         });
     </script>
 </head>
@@ -30,42 +30,46 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header clearfix">
-                        <h2 class="pull-left">Employees Details</h2>
-                        <a href="create.php" class="btn btn-success pull-right">Add New Employee</a>
+                        <h2 class="pull-left">Order Details</h2>
                     </div>
                     <?php
                     // Include config file
                     require_once "config.php";
-                    
+
                     // Attempt select query execution
-                    $sql = "SELECT * FROM employee";
+                    $sql = "SELECT orderNumber, orderDate, orderLineNumber, productName, quantityOrdered, priceEach, productCode FROM orders INNER JOIN orderdetails USING (orderNumber) INNER JOIN products USING (productCode) ORDER BY orderNumber, orderLineNumber;";
+
                     if($result = mysqli_query($link, $sql)){
                         if(mysqli_num_rows($result) > 0){
                             echo "<table class='table table-bordered table-striped'>";
                                 echo "<thead>";
                                     echo "<tr>";
-                                        echo "<th>#</th>";
-                                        echo "<th>Name</th>";
-                                        echo "<th>Address</th>";
-                                        echo "<th>Salary</th>";
-                                        echo "<th>Action</th>";
+                                    echo "<th>Order Number</th>";
+                                    echo "<th>Order Date</th>";
+                                    echo "<th>Order LineNumber</th>";
+                                    echo "<th>Product Number</th>";
+                                    echo "<th>Quantity Ordered</th>";
+                                    echo "<th>Price Each</th>";
+                                    echo "<th>Actions</th>";
                                     echo "</tr>";
                                 echo "</thead>";
                                 echo "<tbody>";
                                 while($row = mysqli_fetch_array($result)){
                                     echo "<tr>";
-                                        echo "<td>" . $row['id'] . "</td>";
-                                        echo "<td>" . $row['name'] . "</td>";
-                                        echo "<td>" . $row['address'] . "</td>";
-                                        echo "<td>" . $row['salary'] . "</td>";
-                                        echo "<td>";
-                                            echo "<a href='read.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
-                                            echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
-                                            echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
-                                        echo "</td>";
+                                    echo "<td>" . $row['orderNumber'] . "</td>";
+                                    echo "<td>" . $row['orderDate'] . "</td>";
+                                    echo "<td>" . $row['orderLineNumber'] . "</td>";
+                                    echo "<td>" . $row['productCode'] . "</td>";
+                                    echo "<td>" . $row['quantityOrdered'] . "</td>";
+                                    echo "<td>" . $row['priceEach'] . "</td>";
+                                    echo "<td>";
+                                        echo "<a href='read.php?id=". $row['orderNumber'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span></a>";
+                                        echo "<a href='update.php?id=". $row['orderNumber'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                        echo "<a href='delete.php?id=". $row['orderNumber'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                    echo "</td>";
                                     echo "</tr>";
                                 }
-                                echo "</tbody>";                            
+                                echo "</tbody>";
                             echo "</table>";
                             // Free result set
                             mysqli_free_result($result);
@@ -75,12 +79,12 @@
                     } else{
                         echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
                     }
- 
+
                     // Close connection
                     mysqli_close($link);
                     ?>
                 </div>
-            </div>        
+            </div>
         </div>
     </div>
 </body>
