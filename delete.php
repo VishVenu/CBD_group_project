3c69799b904485c72859b120fab261e7ad68a741
VishVenu <vishakhasreejit@gmail.com>
2022-05-20 22:49:42 +0530

    merge resolved

--- delete.php
+++ delete.php
@@ -1,19 +1,23 @@
 <?php
 // Process delete operation after confirmation
-if(isset($_POST["id"]) && !empty($_POST["id"])){
+if(isset($_POST['order_number']) && !empty($_POST['order_number']) && isset($_POST['order_line_number']) && !empty($_POST['order_line_number'])){
+
+    $order_number = $_POST['order_number'];
+    $order_line_number = $_POST['order_line_number'];
+
     // Include config file
     require_once "config.php";
     
     // Prepare a delete statement
-    $sql = "DELETE FROM employee WHERE id = ?";
+    $sql = "DELETE FROM orderdetails WHERE orderNumber = ? AND orderLineNumber = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        mysqli_stmt_bind_param($stmt, "ii", $param_order_number, $param_order_line_number);
         
         // Set parameters
-        $param_id = trim($_POST["id"]);
-        
+        $param_order_number = $order_number;
+		$param_order_line_number = $order_line_number;
         // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
             // Records deleted successfully. Redirect to landing page
@@ -30,8 +34,8 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     // Close connection
     mysqli_close($link);
 } else{
-    // Check existence of id parameter
-    if(empty(trim($_GET["id"]))){
+    // Check existence of order parameter
+    if(empty($_GET['order_number']) || empty($_GET['order_line_number'])){
         // URL doesn't contain id parameter. Redirect to error page
         header("location: error.php");
         exit();
@@ -61,7 +65,8 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                     </div>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger fade in">
-                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                            <input type="hidden" name="order_number" value="<?php echo $_GET["order_number"]; ?>"/>
+							<input type="hidden" name="order_line_number" value="<?php echo $_GET["order_line_number"]; ?>"/>
                             <p>Are you sure you want to delete this record?</p><br>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
